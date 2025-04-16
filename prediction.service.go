28df5ec25dbb0507c91d4e806f7ea55a9810bfe4
Andres Campuzano <campuxano88@gmail.com>
2025-04-16 18:24:14 -0500

    Add support for batch creation of weather predictions

--- prediction.service.go
+++ prediction.service.go
@@ -6,37 +6,42 @@ import (
 )
 
 func (server *APIServer) handleCreatePrediction(w http.ResponseWriter, r *http.Request) error {
-	req := new(CreatePredictionRequest)
-	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+	var reqs []CreatePredictionRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqs); err != nil {
 		return err
 	}
 
-	// Verify the city exists
-	_, err := server.store.GetCityByID(req.CityID)
-	if err != nil {
-		return err
+	var createdPredictions []*Prediction
+	for _, req := range reqs {
+		// Verify the city exists
+		_, err := server.store.GetCityByID(req.CityID)
+		if err != nil {
+			return err
+		}
+
+		prediction, err := NewPrediction(
+			req.CityID,
+			req.Temperature,
+			req.Humidity,
+			req.ForecastFor,
+		)
+		if err != nil {
+			return err
+		}
+
+		err = server.store.CreatePrediction(prediction)
+		if err != nil {
+			return err
+		}
+
+		// Recovering prediction from DB
+		createdPrediction, err := server.store.GetPredictionByID(prediction.ID)
+		if err != nil {
+			return err
+		}
+
+		createdPredictions = append(createdPredictions, createdPrediction)
 	}
 
-	prediction, err := NewPrediction(
-		req.CityID,
-		req.Temperature,
-		req.Humidity,
-		req.ForecastFor,
-	)
-	if err != nil {
-		return err
-	}
-
-	err = server.store.CreatePrediction(prediction)
-	if err != nil {
-		return err
-	}
-
-	// Recovering prediction from DB
-	createdPrediction, err := server.store.GetPredictionByID(prediction.ID)
-	if err != nil {
-		return err
-	}
-
-	return WriteJSON(w, http.StatusOK, createdPrediction)
+	return WriteJSON(w, http.StatusOK, createdPredictions)
 }
